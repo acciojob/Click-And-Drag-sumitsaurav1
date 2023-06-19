2492eace8422a2148c0e7570430694ce09d87404
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-06-19 08:59:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 // Your code here.
+const items = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+items.addEventListener('mousedown', (e) => {
+  isDown = true;
+  items.classList.add('active');
+  startX = e.pageX - items.offsetLeft;
+  scrollLeft = items.scrollLeft;
+});
+
+items.addEventListener('mouseleave', () => {
+  isDown = false;
+  items.classList.remove('active');
+});
+
+items.addEventListener('mouseup', () => {
+  isDown = false;
+  items.classList.remove('active');
+});
+
+items.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - items.offsetLeft;
+  const walk = (x - startX) * 2; // Adjust the sliding speed here
+  items.scrollLeft = scrollLeft - walk;
+});
+
+// Disable text selection while dragging
+items.addEventListener('selectstart', (e) => {
+  e.preventDefault();
+});
